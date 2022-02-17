f0741e54f2089695d22106fb9712bea318ad170a
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-02-17 17:43:53 +0200

    payment method done

--- src/pages/Six.js
+++ src/pages/Six.js
@@ -1,8 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import Form  from 'react-bootstrap/Form'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-const Six = ({setPaymentMethod,monthlyPremium,armsOfService}) =>{
+const Six = ({paymentMethod, setPaymentMethod,monthlyPremium,armsOfService}) =>{
+  const [showNext,setShowNext] = useState(false)
+    const [error, setError] = useState('')
+    const requiredSubmitted = () =>{
+      if(paymentMethod != ''){
+        setShowNext(true)
+        setError('Please Check that all info is correct then click next')
+      }else{
+        setError('You have not submitted your Payment Method')
+      }
+        }
   return(
     <div className="page">
 
@@ -24,12 +35,14 @@ const Six = ({setPaymentMethod,monthlyPremium,armsOfService}) =>{
             </Form.Select>
           </Form.Group>
         </Form>
+        <h6 style={{color: 'red', fontWeight: 'bold',float: 'left'}}>{error}</h6>
         <Link to='/5'>
           <Button variant='secondary w-25' style={{ fontWeight: '600', background: '#D0D0D0', border: 'none', float:'left', marginTop: '10vw', marginLeft: '20vw' }}>BACK</Button>
         </Link>
-        <Link to='/7'>
+        {showNext && <Link to='/7'>
           <Button variant='danger w-25' style={{ fontWeight: '600', background: '#BB1A1B', border: 'none', float:'right', marginTop: '10vw', marginRight: '20vw' }} >NEXT</Button>
-        </Link>
+        </Link>}
+        {!showNext && <Button variant='danger w-25' style={{ fontWeight: '600', background: '#BB1A1B', border: 'none', float:'right', marginTop: '10vw', marginRight: '20vw', marginBottom: '10vh' }} onClick={requiredSubmitted} >Submit</Button>}
       </div>
     </div>
   )
