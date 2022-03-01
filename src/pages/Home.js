6edd5c89e02d3f321ebff2e87f68c2002af5fb32
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-03-01 16:05:50 +0200

    Home wording done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = ({idParam,setIdParam}) => {
       
       <p>In South Africa, the Protection of Personal Information Act 4 of 2013 (POPIA) has been enacted to regulate the processing of Personal Information. When entering into this policy you provide us with information that may be protected by legislation including but not limited to POPIA.  We will take all reasonable steps to protect the information you provide us securely and confidentially.</p>					
       
-      <p>I confirm that my information and all information supplied by me, maybe processed and that I have the legal right to supply the necessary information in this application and the legal right to give consent to process the personal/sensitive/special information.	</p>				
+      <p>I confirm that my information and all information supplied by me, may be processed and that I have the legal right to supply the necessary information in this application and the legal right to give consent to process the personal/sensitive/special information.	</p>				
       
       <p>By opting to continue you consent to the use of the information supplied by you for the following reasons:</p>
 
